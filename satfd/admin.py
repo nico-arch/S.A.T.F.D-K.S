64fbd442d1feee143c347c656cf7f3620d54f028
Nick Anderson Azémar <76443393+nico-arch@users.noreply.github.com>
2022-06-24 04:53:23 +0000

    .

--- satfd/admin.py
+++ satfd/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.site_header = "Système Administration"
+admin.site.site_title  = "Système Administration"
+admin.site.index_title = "Administration"
+
+admin.site.register(Lieu_de_travail)
+admin.site.register(Personne)
+
+
+
